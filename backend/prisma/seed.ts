dc538ea553f6103c3422bd960bea2ffd589431f0
Joao Pedro <jpviolao@hotmail.com>
2026-04-23 04:43:14 -0300

    feat(seed): criar arquivo seed.ts para popular o banco de dados
    O QUE FOI FEITO
        - Criado arquivo seed.ts e chamada função upsert (idempotente) para popular o banco de dados

---
+++ backend/prisma/seed.ts
@@ -0,0 +1,24 @@
+import { prisma } from '../src/lib/prisma';
+import { Role } from '../src/generated/prisma';
+
+async function main() {
+  await prisma.user.upsert({
+    where: { email: 'test@gmail.com' },
+    update: {},
+    create: {
+      email: 'test@gmail.com',
+      name: 'John Doe',
+      role: Role.USER,
+    },
+  });
+}
+
+main()
+  .then(async () => {
+    await prisma.$disconnect();
+  })
+  .catch(async (err) => {
+    await prisma.$disconnect;
+    console.log('Error: ' + err);
+    process.exit(1);
+  });

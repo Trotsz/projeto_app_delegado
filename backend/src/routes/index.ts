6e5fbbfa91ccd55146a3b27d4161618d66d8a979
Joao Pedro <jpviolao@hotmail.com>
2026-05-27 02:03:31 -0300

    feat(cadastro/login): implementar cadastro e autenticacao
    **OBJETIVO**
    - Implementar registro e login de usuário
    
    **O QUE FOI FEITO**
    - Implementadas rotas register e login e o resto do padrão Route->Controller->Service->Repository
    - Implementado hash de senha no registro
    - Implementado geração de token JWT

--- backend/src/routes/index.ts
+++ backend/src/routes/index.ts
@@ -1,16 +1,31 @@
 import type { Request, Response } from 'express';
 import express from 'express';
+import bodyParser from 'body-parser';
 import userController from '../controllers/UserController.ts';
 
 const app = express();
 const port = 3000;
 
+const jsonParser = bodyParser.json();
+
 app.get('/', (_: Request, res: Response) => {
   res.send('Root');
 });
 
-app.get('/users', (_, res) => {
+app.post('/user/register', jsonParser, (req: Request, res: Response) => {
+  userController.create(req, res);
+});
+
+app.post('/user/login', jsonParser, (req: Request, res: Response) => {
+  userController.login(req, res);
+});
+
+app.get('/users', (_: any, res: Response) => {
   userController.findAll(_, res);
+});
+
+app.get('/user/:email', (req, res) => {
+  userController.findByEmail(req, res);
 });
 
 app.listen(port, () => {

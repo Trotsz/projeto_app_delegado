9bf9e9606e8c6bcc7b59ca97d552d9532ee8ed61
Joao Pedro <jpviolao@hotmail.com>
2026-05-28 16:00:24 -0300

    test: adicionar zustand, axios e react-query

--- app_conexao_direta/App.tsx
+++ app_conexao_direta/App.tsx
@@ -1,11 +1,16 @@
 import React from 'react';
 import { View, Text, StyleSheet } from 'react-native';
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
+
+const queryClient = new QueryClient();
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text style={styles.text}>App Delegado - Teste</Text>
-    </View>
+    <QueryClientProvider client={queryClient}>
+      <View style={styles.container}>
+        <Text style={styles.text}>App Delegado - Teste</Text>
+      </View>
+    </QueryClientProvider>
   );
 }
 

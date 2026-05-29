c7d049e6ec207738adf25ea15afd8b34ee008c4c
Joao Pedro <jpviolao@hotmail.com>
2026-05-28 23:51:19 -0300

    test: adicionar @react-navigation/native (NavigationContainer)

--- app_conexao_direta/App.tsx
+++ app_conexao_direta/App.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import { View, Text, StyleSheet } from 'react-native';
 import { SafeAreaProvider } from 'react-native-safe-area-context';
+import { NavigationContainer } from '@react-navigation/native';
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 
 const queryClient = new QueryClient();
@@ -9,9 +10,11 @@ export default function App() {
   return (
     <SafeAreaProvider>
       <QueryClientProvider client={queryClient}>
-        <View style={styles.container}>
-          <Text style={styles.text}>App Delegado - Teste</Text>
-        </View>
+        <NavigationContainer>
+          <View style={styles.container}>
+            <Text style={styles.text}>App Delegado - Teste</Text>
+          </View>
+        </NavigationContainer>
       </QueryClientProvider>
     </SafeAreaProvider>
   );

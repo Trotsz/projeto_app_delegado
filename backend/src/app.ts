97bcb2e5b4eb118d1366eaa43f3aee5a12a24c83
Joao Pedro <jpviolao@hotmail.com>
2026-06-02 15:21:36 -0300

    feat(cors): adicionar url do frontend ao cors

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -3,7 +3,7 @@ import cors from 'cors';
 import routes from './routes/index.ts';
 
 const app = express();
-app.use(cors());
+app.use(cors({ origin: ['http://localhost:8081', 'https://projetoappdelegado.vercel.app/'] }));
 app.use(express.json());
 app.use(routes);
 
